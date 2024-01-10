a1a66057825e1ddc2b450051576d3b3b8a00fb57
Richa Jain <jainricha127@gmail.com>
2024-01-10 10:47:05 -0500

    commit filter confounders

--- group_project/bst270Midus2024/R/filter_confounders.R
+++ group_project/bst270Midus2024/R/filter_confounders.R
@@ -1,20 +1,30 @@
-library(tidyverse)
+library("tidyverse")
 
-load("~/Library/Mobile Documents/com~apple~CloudDocs/Harvard/Harvard Winter 2024/BST270/bst270-winter2024/group_project/bst270Midus2024/data/29282-0001-Data.rda")
-load("~/Library/Mobile Documents/com~apple~CloudDocs/Harvard/Harvard Winter 2024/BST270/bst270-winter2024/group_project/bst270Midus2024/data/04652-0001-Data.rda")
+#' Clean and filter confounders
+#' 
+#' @param df A dataframe.
+#' @param group2_columns Confounder columns.
+#' @examples
+#' dat <- data.frame(sample_data)
+#' filter_confounders(dat)
 
-data = inner_join(da04652.0001, da29282.0001, by = c("M2ID", "M2FAMNUM"),suffix = c('','.2'))
-
-columns <- c('B1PB1','B1STINC1','B4ZB1SLG','B1SCHROX','B4H26','B4H33','B4H25','B4PBMI','B1SNEGAF')
-
-#filter_confounders <- function(data){
-#  res <- data %>% 
-#    select(columns) %>% 
-#    filter(!is.na()) 
-#}
-
-test <- data %>% drop_na(any_of(columns))
-
-table(test$B1PB1)
-
+filter_confounders <- function(df, group2_columns) {
+  filt_df <- df %>%
+    drop_na(any_of(group2_columns)) %>%
+    dplyr::filter(B1STINC1 != -1 & B1STINC1 != 9999998) %>%
+    dplyr::filter((B4H26 != "Missing (8)") & (B4H26 != "Inapplicable (9)")) %>%
+    # Converting education levels to match paper
+    dplyr::mutate(B1PB1 = dplyr::case_when(
+      grepl("(08)|(09)|(10)|(11)|(12)", B1PB1) ~ 4, # College or more
+      grepl("(06)|(07)", B1PB1) ~ 3, # Some college
+      grepl("(05)", B1PB1) ~ 2, # High school degree
+      grepl("(01)|(02)|(03)|(04)", B1PB1) ~ 1 # Less than high school degree
+    )) %>%
+    dplyr::mutate(B1SCHROX = ifelse(grepl("No", B1SCHROX), 0, 1), # Changing chronic conditions
+                  B4H26 = ifelse(grepl("No", B4H26), 0, 1), # Changing smoking
+                  B4H33 = ifelse(grepl("No", B4H33), 0, 1), # Changing alcohol consumption
+                  B4H25 = ifelse(grepl("No", B4H25), 0, 1)) # Changing regular exercise
   
+  return(filt_df)
+}
+
